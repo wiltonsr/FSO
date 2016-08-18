68281f5987514d4eb47fd42b0ee76cd29eac3d4d
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-18 19:37:39 -0300

    Adding method to get the points of the triangle
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- triangle_calculator/io.c
+++ triangle_calculator/io.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "io.h"
+#include "triangle.h"
 
-// Define functions
+Point *get_point(int point_number){
+    Point *point = malloc(sizeof(Point));
+    int x, y;
+
+    printf("Put coordinates X and Y of the point %d (0 0):\n", point_number);
+    scanf("%d %d", &x, &y);
+    point->x = x;
+    point->y = y;
+
+    return point;
+}
+
+Triangle *get_triangle(){
+    printf("We will get 3 points form your triangle\n");
+    Triangle *triangle = malloc(sizeof(Triangle));
+
+    int i;
+    for (i = 0; i < 3; i++) {
+        triangle->point[i] = get_point(i);
+    }
+
+    return triangle;
+}
