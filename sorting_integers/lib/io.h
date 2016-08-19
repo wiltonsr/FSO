265f017439e985c80f711644ec888019336842d2
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-19 17:05:44 -0300

    Adding headers to io.h
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- sorting_integers/lib/io.h
+++ sorting_integers/lib/io.h
@@ -1,5 +1,11 @@
 #ifndef IO_H
 #define IO_H
+#include "sort_list.h"
 
-// Adding headers
+Sort_list get_list(int argc, int *argv[]);
+
+int validate_argc(int argc);
+
+void print_error(char *menssage);
+
 #endif
