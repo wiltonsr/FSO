bdd3cb5f6d64267d186ac416857504d4572f1052
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-19 08:35:18 -0300

    Adding calc to area
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- triangle_calculator/operations.c
+++ triangle_calculator/operations.c
@@ -2,8 +2,15 @@
 #include "triangle.h"
 #include "operations.h"
 
-double calculate_area(Triangle triangle){
-    return 0.0;
+double calculate_area(Triangle *triangle){
+    Point *a = triangle->point[0];
+    Point *b = triangle->point[1];
+    Point *c = triangle->point[2];
+    double area = 0.0;
+
+    area = (a->x * (b->y - c->y) + b->x * (c->y - a->y) + c->x * (a->y - b->y))/2;
+
+    return area;
 }
 
 double calculate_sides(Triangle triangle){
