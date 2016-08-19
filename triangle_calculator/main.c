ac8ee48a37e3cf95715df7dc10a9d8893d8dd36d
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-19 10:19:55 -0300

    Adding method to calculate and print perimenter on main
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- triangle_calculator/main.c
+++ triangle_calculator/main.c
@@ -7,7 +7,10 @@ int main(void){
     Triangle *triangle = get_triangle();
     double area = calculate_area(triangle);
     double *sides = calculate_sides(triangle);
+    double perimeter = calculate_perimeter(triangle);
 
     print_area(area);
+    print_len_sides(sides);
+    print_perimeter(perimeter);
     return 0;
 }
