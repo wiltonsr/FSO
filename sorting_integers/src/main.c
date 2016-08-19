ddaa4e65e0edcc9ee8840ee5302fd84731a7f4da
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-19 16:52:10 -0300

    Adding args on main
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- sorting_integers/src/main.c
+++ sorting_integers/src/main.c
@@ -1,7 +1,13 @@
 #define _GNU_SOURCE
 
-int main(void){
+int main(int argc, char *argv[]){
 
-    /* Implemente here */
+    int is_valid = validate_argc(argc);
+    if(is_valid){
+        /* Implemente here */
+    }
+    else{
+
+    }
     return 0;
 }
