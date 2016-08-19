bcbf305d37e952cfd3942c5e7faf8233bc716f3c
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-19 16:52:22 -0300

    Verify if argc is valid
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- sorting_integers/src/io.c
+++ sorting_integers/src/io.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../lib/io.h"
+#include "../lib/sort_list.h"
 
+Sort_list get_list(int argc, int *argv[]){
+}
+
+int validate_argc(int argc){
+    int is_valid = 0;
+
+    if (argc >= 2)
+        is_valid = 1;
+
+    return is_valid;
+}
