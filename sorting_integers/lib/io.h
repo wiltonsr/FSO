c239446042588183adc2b4eeba9b7f32c1e72d7f
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-22 17:54:28 -0300

    Adding comments to io header
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- sorting_integers/lib/io.h
+++ sorting_integers/lib/io.h
@@ -1,18 +1,26 @@
 #ifndef IO_H
 #define IO_H
 
+// Validate the number of the argc
 int validate_argc(int argc);
 
+// Print a given error
 void print_error(const char *menssage);
 
+// Print the man of the software
 void print_man();
 
+// Check if the argv is correct
 int check_argc_argv(int argc, int flag);
 
+// Validate all argv
 int validate_argv(int argc, char *argv[]);
 
+// Global messages to send to print error
 extern const char *MISSING_ARG;
 extern const char *MISSING_INTEGER;
+
+// Flags to identify types of args
 extern const int EMPTY_ARG;
 extern const int CRESCENT_ARG;
 extern const int DECREASIN_ARG;
