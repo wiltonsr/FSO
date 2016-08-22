359007333d2fc1829631676fb13af24fb98da79b
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-22 14:43:24 -0300

    Adding method to find the flag
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- sorting_integers/lib/core.h
+++ sorting_integers/lib/core.h
@@ -1,7 +1,11 @@
 #ifndef CORE_H
 #define CORE_H
+#include <stdlib.h>
 #include "sort_list.h"
+#include "../lib/io.h"
 
-Sortlist *get_sort_list(const int TYPE, int argc, char *argv[]);
+Sortlist *get_sort_list(const int TYPE, const int argc, char *argv[]);
+
+int find_flag(const int FLAG, const int argc, char *argv[]);
 
 #endif
--- sorting_integers/src/core.c
+++ sorting_integers/src/core.c
@@ -1,10 +1,29 @@
 /* Core of the sort program, with array manipulation, etc */
-#include <stdlib.h>
 #include "../lib/core.h"
-#include "../lib/io.h"
+#include <string.h>
 
-Sortlist *get_sort_list(const int TYPE, int argc, char *argv[]){
-    Sortlist *sort_list = (Sortlist *)malloc(sizeof(Sortlist));
+Sortlist *get_sort_list(const int flag, const int argc, char *argv[]){
+    Sortlist *sort_list = malloc(sizeof(Sortlist));
+    int position_flag = 0;
+
+    if(flag){
+        position_flag = find_flag(flag, argc, argv);
+    }
     return sort_list;
 }
 
+int find_flag(const int flag, const int argc, char *argv[]){
+    int position_flag = 0;
+
+    for (int i = 0; i < argc; i++) {
+        if(flag == CRESCENT_ARG){
+            if (strcmp(argv[i], "-d"))
+                position_flag = i;
+        }
+        else if(flag ==  DECREASIN_ARG){
+            if (strcmp(argv[i], "-r"))
+                position_flag = i;
+        }
+    }
+    return position_flag;
+}
