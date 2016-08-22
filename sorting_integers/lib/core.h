565ebf6e6000452b48722ddc7fe5b94ddf5daa45
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-22 12:02:38 -0300

    Adding core files

---
+++ sorting_integers/lib/core.h
@@ -0,0 +1,7 @@
+#ifndef CORE_H
+#define CORE_H
+#include "sort_list.h"
+
+Sortlist *get_sort_list(const int TYPE, int argc, int *argv[]);
+
+#endif
---
+++ sorting_integers/src/core.c
@@ -0,0 +1,10 @@
+/* Core of the sort program, with array manipulation, etc */
+#include <stdlib.h>
+#include "../lib/core.h"
+#include "../lib/io.h"
+
+Sortlist *get_sort_list(const int TYPE, int argc, int *argv[]){
+    Sortlist *sort_list = (Sortlist *)malloc(sizeof(Sortlist));
+    return sort_list;
+}
+
