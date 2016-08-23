54ec5b0bc41757b6e5423834fa99d85c49d7c797
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-22 21:37:17 -0300

    Adding method to compare and sort the list
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- sorting_integers/lib/sort.h
+++ sorting_integers/lib/sort.h
@@ -1,7 +1,11 @@
-#ifndef CORE_H
-#define CORE_H
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdlib.h>
 #include "sort_list.h"
+
+int compare(const void * rigth, const void * left);
+
+Sortlist *make_sort(Sortlist *sort_list);
 
 #endif
--- sorting_integers/src/sort.c
+++ sorting_integers/src/sort.c
@@ -1,2 +1,19 @@
 /* Make a sorting with the integers */
 #include "../lib/core.h"
+
+int compare(const void * rigth, const void * left){
+    int f = *((int*)rigth);
+    int s = *((int*)left);
+
+    if (f > s) return  1;
+    if (f < s) return -1;
+    return 0;
+}
+
+Sortlist *make_sort(Sortlist *sort_list){
+    int *list = sort_list->int_list;
+    qsort(list, sort_list->list_len, sizeof(int), compare);
+    sort_list->int_list = list;
+
+    return sort_list;
+}
