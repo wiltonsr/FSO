cb1d5ac87cf11352a71c1ed9ac84b965024a2363
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-23 13:35:28 -0300

    Adding a manupulatio to pointers with number
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- pointer_manipulation/src/main.c
+++ pointer_manipulation/src/main.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 
 void make_manipulation(){
+    double number1 = 7.3, number2;
+    char s1[100], s2[100];
 
+    double *dPtr;
+    dPtr = &number1;
+    printf("%lf\n", *dPtr);
+    number2 = *dPtr;
+    printf("%lf\n", number2);
+    printf("%p\n", &number1);
+    printf("%p\n", dPtr);
+
+    /* printf("%d\n", *dPtr); */
 }
 
 int main(void){
