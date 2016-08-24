14a1033ac762931852865a912a910ee798d9d1ba
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-23 21:44:59 -0300

    Comment the code with explanations
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- pointer_manipulation/src/main.c
+++ pointer_manipulation/src/main.c
@@ -6,21 +6,35 @@ void make_manipulation(){
     double number1 = 7.3, number2;
     char s1[100], s2[100];
 
+    // Declare pointer
     double *dPtr;
+
+    // Point to valor of number1
     dPtr = &number1;
     printf("Value of dPtr: %lf\n", *dPtr);
+
+    // Number2 is equals to pointer value
     number2 = *dPtr;
     printf("Value of number2: %lf\n", number2);
     printf("Pointer to number1: %p\n", &number1);
     printf("Pointer to dPtr: %p\n", dPtr);
 
+    // Get a string
     printf("Put a string: ");
     scanf("%s", &s1);
+
+    // Copy the value of the s1 to s2
     strcpy(s2, s1);
+
+    // Compare the value of the s1 and s2
     int compare = strcmp(s1, s2);
     printf("Compare to s1 and s2: %d\n", compare);
+
+    // Concat the value of the s1 and s2
     strcat(s1, s2);
     printf("Concat of s1 and s2: %s\n", s1);
+
+    // Calculate the len of the s1
     int len = strlen(s1);
     printf("Len of |%s| is |%d|\n", s1, len);
 
