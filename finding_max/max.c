c14be3cc6ea227f1de91c83fa6f342e0bbe2c501
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-09 09:36:43 -0300

    Adding a system to compare the integers
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- finding_max/max.c
+++ finding_max/max.c
@@ -24,75 +24,80 @@ typedef struct pair_compare_t{
 /* } */
 
 void * compare( void * args ){
-  pair_compare_t * pair_compare = (pair_compare_t*) args;
-  if (pair_compare->left->x < pair_compare->right->x)
-    pair_compare->left->w = 0;
-  else
-    pair_compare->right->w = 0;
-
-  printf("pasosu\n");
+    pair_compare_t * pair_compare = (pair_compare_t*) args;
+    if (pair_compare->left->x < pair_compare->right->x)
+        pair_compare->left->w = 0;
+    else
+        pair_compare->right->w = 0;
 }
 
 void * initialize_one( void * args ){
-  Array *thread_args = (Array*) args;
-  thread_args->w = 1;
+    Array *thread_args = (Array*) args;
+    thread_args->w = 1;
 }
 
 int main(int argc, char *argv[])
 {
-  int NUM_INITIALIZER = argc - 1;
-  int NUM_COMPARATIONS= ((argc - 1)*(argc - 2))/2;
-  pthread_t thread[NUM_INITIALIZER];
+    int NUM_INITIALIZER = argc - 1;
+    int NUM_COMPARATIONS= ((argc - 1)*(argc - 2))/2;
+    pthread_t thread[NUM_INITIALIZER];
 
-  Array *thread_args = malloc(NUM_INITIALIZER * sizeof(Array));
+    Array *thread_args = malloc(NUM_INITIALIZER * sizeof(Array));
 
-  for(int i = 0; i < NUM_INITIALIZER; i++){
-    thread_args[i].x = atoi(argv[i+1]);
-  }
+    for(int i = 0; i < NUM_INITIALIZER; i++){
+        thread_args[i].x = atoi(argv[i+1]);
+    }
 
-  for(int i = 0; i < NUM_INITIALIZER; i++){
-    thread_args[i].n = i;
-    pthread_create(&thread[i], NULL, initialize_one, &thread_args[i]);
-  }
+    for(int i = 0; i < NUM_INITIALIZER; i++){
+        thread_args[i].n = i;
+        pthread_create(&thread[i], NULL, initialize_one, &thread_args[i]);
+    }
 
 
-  for(int i = 0; i < NUM_INITIALIZER; i++){
-    pthread_join(thread[i], NULL);
-  }
+    for(int i = 0; i < NUM_INITIALIZER; i++){
+        pthread_join(thread[i], NULL);
+    }
 
-  pair_compare_t *pair_compare = malloc(NUM_COMPARATIONS * sizeof(pair_compare_t));
-  int count_compare = 0;
+    pair_compare_t *pair_compare = malloc(
+            NUM_COMPARATIONS * sizeof(pair_compare_t)
+            );
 
-  for (int i = 0; i < NUM_INITIALIZER - 1; i++) {
-      for (int j = i + 1; j < NUM_INITIALIZER; j++) {
-        pair_compare[count_compare].left = &thread_args[i];
-        pair_compare[count_compare].right = &thread_args[j];
-        count_compare++;
+    for (int i = 0; i < NUM_INITIALIZER - 1; i++) {
+        int count_compare = 0;
+        for (int j = i + 1; j < NUM_INITIALIZER; j++) {
+            pair_compare[count_compare].left = &thread_args[i];
+            pair_compare[count_compare].right = &thread_args[j];
 
-        pthread_create(&thread[i], NULL, compare, &pair_compare);
-      }
-  }
+            pthread_create(
+                    &thread[i],
+                    NULL,
+                    compare,
+                    &pair_compare[count_compare]
+                    );
+            count_compare++;
+        }
+    }
 
-  for(int i = 0; i < NUM_INITIALIZER; i++){
-    pthread_join(thread[i], NULL);
-  }
+    for(int i = 0; i < NUM_INITIALIZER; i++){
+        pthread_join(thread[i], NULL);
+    }
 
-  for (int i = 0; i < NUM_INITIALIZER; i++) {
-      printf("w = %d\n", thread_args[i].w);
-  }
-  /* for(int i = 0; i < NUM_INITIALIZER; i++){ */
-  /*   pthread_create(&thread[i], NULL, compare, (void *)(&x[i])); */
-  /*   pthread_join(thread[i], NULL); */
-  /* } */
-  /*  */
-  /* for(int i = 0; i < NUM_INITIALIZER; i++){ */
-  /*   printf("%d\n", w[i]); */
-  /* } */
-  /*  */
-  /* for(int i = 0; i < NUM_INITIALIZER; i++){ */
-  /*   pthread_create(&thread[i], NULL, print, (void *)(&i)); */
-  /*   pthread_join(thread[i], NULL); */
-  /* } */
-  /*  */
-  return 0;
+    for (int i = 0; i < NUM_INITIALIZER; i++) {
+        printf("w = %d\n", thread_args[i].w);
+    }
+    /* for(int i = 0; i < NUM_INITIALIZER; i++){ */
+    /*   pthread_create(&thread[i], NULL, compare, (void *)(&x[i])); */
+    /*   pthread_join(thread[i], NULL); */
+    /* } */
+    /*  */
+    /* for(int i = 0; i < NUM_INITIALIZER; i++){ */
+    /*   printf("%d\n", w[i]); */
+    /* } */
+    /*  */
+    /* for(int i = 0; i < NUM_INITIALIZER; i++){ */
+    /*   pthread_create(&thread[i], NULL, print, (void *)(&i)); */
+    /*   pthread_join(thread[i], NULL); */
+    /* } */
+    /*  */
+    return 0;
 }
