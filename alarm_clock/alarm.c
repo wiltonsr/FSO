379a165a960cf2d3f61fc81bf4eeab8db4f3aeb6
Wilton Rodrigues <braynwilton@gmail.com>
2016-09-10 10:45:50 -0300

    Fixes q01

--- alarm_clock/alarm.c
+++ alarm_clock/alarm.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/wait.h>
+
+void handler(int signal_number){
+  printf("Son finished\n");
+}
 
 int main(void)
 {
@@ -8,13 +13,15 @@ int main(void)
 
   if (pid_son == 0){ //The son process was sucessfuly created
     sleep(5); //Wait 5 second
-    kill(pid_son, SIGALRM); //Kill the son process and send a SIGALRM
+    kill(getppid(), SIGALRM); //Kill the son process and send a SIGALRM
   }
   else if (pid_son < 0)
     perror("Error on fork call"); //The fork doesnt work
   else{ //The father process
     printf("Waiting for the SIGALRM from my son!\n");
+    signal(SIGALRM, handler);
     pause(); //Wait for the SIGALRM from the son process
+    printf("The SIGALRM arrived!\n");
   }
 
   return 0;
