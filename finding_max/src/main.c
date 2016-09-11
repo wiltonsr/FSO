920ed4767279813b2b00bab958f23fc4424f7436
Wilton Rodrigues <braynwilton@gmail.com>
2016-09-11 12:40:11 -0300

    Adding outputs request in q02

--- finding_max/src/main.c
+++ finding_max/src/main.c
@@ -10,50 +10,76 @@
 
 int main(int argc, char *argv[])
 {
-    int NUM_INITIALIZER = argc - 1;
-    int NUM_COMPARATIONS= ((argc - 1)*(argc - 2))/2;
-    pthread_t thread[NUM_INITIALIZER];
+  int NUM_INITIALIZER = argc - 1;
+  int NUM_COMPARATIONS= ((argc - 1)*(argc - 2))/2;
+  pthread_t thread[NUM_INITIALIZER];
 
-    Array *thread_args = malloc(NUM_INITIALIZER * sizeof(Array));
+  Array *thread_args = malloc(NUM_INITIALIZER * sizeof(Array));
+
+  for(int i = 0; i < NUM_INITIALIZER; i++){
+    thread_args[i].x = atoi(argv[i+1]);
+    thread_args[i].n = i;
+  }
+
+  printf("Number of input values = %d\n", NUM_INITIALIZER);
+  if(NUM_INITIALIZER > 0){
+    printf("Input values x = ");
+    for(int i = 0; i < NUM_INITIALIZER; i++){
+      printf("%d ",thread_args[i]);
+    }
+    printf("\n");
 
     for(int i = 0; i < NUM_INITIALIZER; i++){
-        thread_args[i].x = atoi(argv[i+1]);
+      thread_args[i].n = i;
+      pthread_create(&thread[i], NULL, initialize_one, &thread_args[i]);
     }
-
-    for(int i = 0; i < NUM_INITIALIZER; i++){
-        thread_args[i].n = i;
-        pthread_create(&thread[i], NULL, initialize_one, &thread_args[i]);
-    }
-
 
     make_join(thread, NUM_INITIALIZER);
+
+    printf("After initialization w = ");
+    for(int i = 0; i < NUM_INITIALIZER; i++){
+      printf("%d ",thread_args[i].w);
+    }
+    printf("\n");
 
     pair_compare_t *pair_compare = malloc(
-            NUM_COMPARATIONS * sizeof(pair_compare_t)
-            );
+        NUM_COMPARATIONS * sizeof(pair_compare_t)
+        );
 
     for (int i = 0; i < NUM_INITIALIZER - 1; i++) {
-        int count_compare = 0;
-        for (int j = i + 1; j < NUM_INITIALIZER; j++) {
-            pair_compare[count_compare].left = &thread_args[i];
-            pair_compare[count_compare].right = &thread_args[j];
-
-            pthread_create(
-                    &thread[i],
-                    NULL,
-                    compare,
-                    &pair_compare[count_compare]
-                    );
-            count_compare++;
-        }
+      int count_compare = 0;
+      for (int j = i + 1; j < NUM_INITIALIZER; j++) {
+        pair_compare[count_compare].left = &thread_args[i];
+        pair_compare[count_compare].right = &thread_args[j];
+        pthread_create(
+            &thread[i],
+            NULL,
+            compare,
+            &pair_compare[count_compare]
+            );
+        int w_position;
+        if(thread_args[i].x < thread_args[j].x)
+          w_position = i;
+        else
+          w_position = j;
+        printf("Thread T(%d,%d) compares x[%d] = %d and x[%d] = %d, and writes 0 into w[%d]\n", i, j, i, thread_args[i].x, j, thread_args[j].x, w_position);
+        count_compare++;
+      }
     }
 
     make_join(thread, NUM_INITIALIZER);
+
+    printf("After Step 2\nw = ");
+    for(int i = 0; i < NUM_INITIALIZER; i++){
+      printf("%d ",thread_args[i].w);
+    }
+    printf("\n");
 
     for(int i = 0; i < NUM_INITIALIZER; i++){
       pthread_create(&thread[i], NULL, print, &thread_args[i]);
     }
 
     make_join(thread, NUM_INITIALIZER);
-    return 0;
+  }
+  return 0;
 }
--- finding_max/src/max.c
+++ finding_max/src/max.c
@@ -1,26 +1,28 @@
 #include "max.h"
 
 void make_join(pthread_t *threads, int number_threads){
-    for(int i = 0; i < number_threads; i++){
-        pthread_join(threads[i], NULL);
-    }
+  for(int i = 0; i < number_threads; i++){
+    pthread_join(threads[i], NULL);
+  }
 }
 
 void * print( void * args ){
   Array *val= (Array*) args;
-  if (val->w == 1)
-    printf("%d\n", val->x);
+  if (val->w == 1){
+    printf("Maximum = %d\n", val->x);
+    printf("Location = %d\n", val->n);
+  }
 }
 
 void * compare( void * args ){
-    pair_compare_t * pair_compare = (pair_compare_t*) args;
-    if (pair_compare->left->x < pair_compare->right->x)
-        pair_compare->left->w = 0;
-    else
-        pair_compare->right->w = 0;
+  pair_compare_t * pair_compare = (pair_compare_t*) args;
+  if (pair_compare->left->x < pair_compare->right->x)
+    pair_compare->left->w = 0;
+  else
+    pair_compare->right->w = 0;
 }
 
 void * initialize_one( void * args ){
-    Array *thread_args = (Array*) args;
-    thread_args->w = 1;
+  Array *thread_args = (Array*) args;
+  thread_args->w = 1;
 }
