cd7eedf85d7153773d6467df51207ec9384e72b0
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-10 22:37:40 -0300

    Adding a method to calculate
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- matrix_multiplication/main.c
+++ matrix_multiplication/main.c
@@ -12,7 +12,41 @@ int b[2][3] = {
    {4, 5, 6},
 };
 
-int main(int argc, const char *argv[])
-{
+int c[3][3] = {
+   {0, 0, 0},
+   {0, 0, 0},
+   {0, 0, 0},
+};
+
+int get_sum(int i, int j){
+    int sum_elem = 0;
+
+    sum_elem = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+    return sum_elem;
+}
+
+void iterate_matrix(){
+    int result = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            result = get_sum(i, j);
+            c[i][j] = result;
+        }
+    }
+}
+
+void print_matrix(){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, const char *argv[]){
+    iterate_matrix();
+    print_matrix();
     return 0;
 }
