58d220e276b499c6c38e6b6e264dbbc5012cf999
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-10 21:20:38 -0300

    Adding a system to multiple matrix
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

---
+++ matrix_multiplication/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a[3][2] = {
+   {0, 1,},
+   {4, 5,},
+   {8, 9,}
+};
+
+int b[2][3] = {
+   {0, 1, 2},
+   {4, 5, 6},
+};
+
+int main(int argc, const char *argv[])
+{
+    return 0;
+}
