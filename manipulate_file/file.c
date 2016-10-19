8b0cd60402b7919da5f2217f3bbd09e55c94920e
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-10-19 10:49:16 -0300

    missing bkp file

--- manipulate_file/file.c
+++ manipulate_file/file.c
@@ -1,21 +1,13 @@
 #include <time.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <utime.h>
 #include <sys/types.h>
 #include <sys/time.h>
 
-int main(int argc, char *argv[]) {
-  struct stat file_stats;
-
-  if(argc != 2)
-    fprintf(stderr, "Use: ./run file.name AAAAMMDDHHmm\n"), exit(EXIT_FAILURE);
-
-  if((stat(argv[1], &file_stats)) == -1) {
-    perror("fstat");
-    return 1;
-  }
-
+void print_file(struct stat file_stats, char *argv[]){
   printf("Filename: %s\n", argv[1]);
   printf("Time of last modification: %s", ctime(&file_stats.st_mtime));
   printf("File Permissions: ");
@@ -29,6 +21,65 @@ int main(int argc, char *argv[]) {
   printf((file_stats.st_mode & S_IROTH) ? "r" : "-");
   printf((file_stats.st_mode & S_IWOTH) ? "w" : "-");
   printf((file_stats.st_mode & S_IXOTH) ? "x" : "-");
-  printf("\n");
+  printf("\n\n");
+}
+
+int main(int argc, char *argv[]) {
+  /* change name */
+  struct stat file_stats;
+  time_t mtime;
+  struct utimbuf new_times;
+  struct tm new_date = {0};
+
+  char *full_date = malloc(13*sizeof(char));
+  strcpy(full_date, argv[2]);
+
+  char year_string[5];
+  char month_string[3];
+  char day_string[3];
+  char hour_string[3];
+  char minute_string[3];
+
+  memcpy(year_string, &full_date[0], 4);
+  year_string[4] = '\0';
+
+  memcpy(month_string, &full_date[4], 6);
+  month_string[2] = '\0';
+
+  memcpy(day_string, &full_date[6], 8);
+  day_string[2] = '\0';
+
+  memcpy(hour_string, &full_date[8], 10);
+  hour_string[2] = '\0';
+
+  memcpy(minute_string, &full_date[10], 12);
+  minute_string[2] = '\0';
+
+  new_date.tm_year = atoi(year_string);
+  new_date.tm_mon = atoi(month_string);
+  new_date.tm_mday = atoi(day_string);
+
+  /* Limitations on hour of the summer */
+  new_date.tm_hour = atoi(hour_string);
+  new_date.tm_min = atoi(minute_string);
+
+  mtime = mktime(&new_date);
+
+  if(argc != 3)
+    fprintf(stderr, "Use: ./run file.name AAAAMMDDHHmm\n"), exit(EXIT_FAILURE);
+
+  if((stat(argv[1], &file_stats)) == -1) {
+    perror("fstat");
+    return 1;
+  }
+
+  print_file(file_stats, argv);
+
+  new_times.actime = mtime;
+  new_times.modtime = mtime;
+  utime(argv[1], &new_times);
+  stat(argv[1], &file_stats);
+
+  print_file(file_stats, argv);
   return 0;
 }
