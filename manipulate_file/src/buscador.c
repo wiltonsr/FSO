5652e982bd32130a4b8a52d27ee4c423020251b4
Wilton Rodrigues <braynwilton@gmail.com>
2016-10-22 16:18:34 -0200

    Correcting identation

--- manipulate_file/src/buscador.c
+++ manipulate_file/src/buscador.c
@@ -8,57 +8,57 @@
 int count = 0;
 
 void print_30_bytes(char path[256], char word[256], const int max_words){
-    if (count == max_words) return;
-    if (!strstr(path, word)) return;
+  if (count == max_words) return;
+  if (!strstr(path, word)) return;
 
-    FILE* sigD = fopen(path, "r");
-    char *sig[30];
-    fread(sig, 1, 30, sigD);
-    count++;
-    printf("%d. %s --\n", count, path);
-    printf("\t%s\n", sig);
+  FILE* sigD = fopen(path, "r");
+  char *sig[30];
+  fread(sig, 1, 30, sigD);
+  count++;
+  printf("%d. %s --\n", count, path);
+  printf("\t%s\n", sig);
 }
 
 void navigate_dir(char path[], char word[256], const int max_words){
 
-    DIR *d;
-    struct dirent *dir;
-    d = opendir(path);
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(path);
 
-    if (d){
-	while ((dir = readdir(d)) != NULL)
-	{
-	    char dir_name[256] = "";
-	    strcat(dir_name, path);
-	    strcat(dir_name, dir->d_name);
+  if (d){
+    while ((dir = readdir(d)) != NULL)
+    {
+      char dir_name[256] = "";
+      strcat(dir_name, path);
+      strcat(dir_name, dir->d_name);
 
-	    if(!(strcmp(dir->d_name, ".") == 0)){
-		if(!(strcmp(dir->d_name, "..") == 0)){
+      if(!(strcmp(dir->d_name, ".") == 0)){
+        if(!(strcmp(dir->d_name, "..") == 0)){
 
-		    if(dir->d_type == 4){
-			strcat(dir_name, "/");
-			navigate_dir(dir_name, word, max_words);
-		    }
-		    else{
-			print_30_bytes(dir_name, word, max_words);
-		    }
-		}
-	    }
-	}
-	closedir(d);
+          if(dir->d_type == 4){
+            strcat(dir_name, "/");
+            navigate_dir(dir_name, word, max_words);
+          }
+          else{
+            print_30_bytes(dir_name, word, max_words);
+          }
+        }
+      }
     }
+    closedir(d);
+  }
 }
 
 int main(int argc, char *argv[])
 {
 
-    if(argc != 4){
-      printf("Use: ./buscador /path/to/want word_to_seek number_max\n");
-      exit(EXIT_FAILURE);
-    }
-    const int max_words = atoi(argv[3]);
+  if(argc != 4){
+    printf("Use: ./buscador /path/to/want word_to_seek number_max\n");
+    exit(EXIT_FAILURE);
+  }
+  const int max_words = atoi(argv[3]);
 
-    navigate_dir(argv[1], argv[2], max_words);
+  navigate_dir(argv[1], argv[2], max_words);
 
-    return 0;
+  return 0;
 }
