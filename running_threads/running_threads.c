c4ae2182fe932143bd6e7e594bf494ea338e22a4
Wilton Rodrigues <braynwilton@gmail.com>
2016-11-16 11:51:47 -0200

    trab5

--- running_threads/running_threads.c
+++ running_threads/running_threads.c
@@ -1,19 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-int flag = 0;
+#include <unistd.h>
+#include<pthread.h>
+
+int PRINT_FLAG = 0;
+int TERMINATE_FLAG = 0;
+int const ASCII_START = 64;
+pthread_mutex_t lock;
+pthread_mutex_t lock;
 
 void set_end_flag(int sig){
-  flag = 1;
+  TERMINATE_FLAG = 1;
 }
 
-int main(){
-  signal(SIGINT, set_end_flag); 
-  while(1)  
-    if(flag){
-        printf("\nEncerrando a aplicação. Aguardando finalização de threads....\n");
-        printf("\nAplicação encerrada com sucesso!\n Estatísticas: thread 1: XX linhas\n thread 2: YY linhas\n");
-        return 0;
-    }     
+struct info_thread_t{
+  int number;
+  int qtd_lines;
+};
+
+void *print_line(void *args){
+  struct info_thread_t *info = (struct info_thread_t *) args;
+  pthread_mutex_lock(&lock);
+  while(!TERMINATE_FLAG){
+    PRINT_FLAG = 1;
+    for (int i = 0; i < info->number; i++){
+      printf("%c", ASCII_START + info->number);
+    }
+    printf("\n");
+    fflush(stdout);
+    PRINT_FLAG = 0;
+    pthread_mutex_unlock(&lock);
+    info->qtd_lines++;
+    sleep(0.5);
+  }
+}
+
+int main(int argc, char *argv[]){
+
+  if(argc != 2 || ((atoi(argv[1])) > 10) || ((atoi(argv[1])) < 1)){
+    printf("Usage Error\n"
+        "Use: ./run qtd_threads\n"
+        "Where qtd_threads must be a integer between 1 and 10\n");
+    exit(EXIT_FAILURE);
+  }
+
+  int MAX_THREAD = atoi(argv[1]);
+
+  pthread_t thread[MAX_THREAD];
+  struct info_thread_t info_thread[MAX_THREAD];
+
+  for (int i = 0; i < MAX_THREAD; i++){
+    info_thread[i].number = i;
+    info_thread[i].qtd_lines = 0;
+  }
+
+  if (pthread_mutex_init(&lock, NULL) != 0){
+    printf("Failed to init mutex\n");
+    return EXIT_FAILURE;
+  }
+
+  for (int i = 0; i < MAX_THREAD; i++){
+    pthread_create(&thread[i], NULL, &print_line, &info_thread[i]);
+  }
+
+  while(!TERMINATE_FLAG){
+    signal(SIGINT, set_end_flag);
+  }
+
+  if(TERMINATE_FLAG){
+    printf("\nEncerrando a aplicação. Aguardando finalização de threads....\n");
+    for (int i = 0; i < MAX_THREAD; i++){
+      pthread_join(thread[i], NULL);
+    }
+    printf("Aplicação encerrada com sucesso!\n"
+        "Estatísticas: \n");
+    for (int i = 0; i < MAX_THREAD; i++){
+      printf("Thread %d: %d linhas\n", i, info_thread[i].qtd_lines);
+    }
+    return 0;
+  }
+  pthread_mutex_destroy(&lock);
   return 1;
-}  
+}
